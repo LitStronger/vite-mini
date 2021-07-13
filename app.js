231fcb14caa0f727beae536d2b391f73efdf1582
LitStronger <1589015065@qq.com>
2021-07-13 21:01:51 +0800

    调整项目结构

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = new Koa();
 
 const path = require("path");
 const fs = require("fs");
+const { dirname } = require("path");
 
 app.use(async (ctx) => {
   const url = ctx.request.url;
+  // 文件入口
   if (url === "/") {
     ctx.type = "text/html";
     ctx.body = fs.readFileSync("./index.html", "utf-8");
-  } else if (url.endsWith(".js")) {
-    // 获取js文件绝对路径，读取并返回
+  } 
+  else if (url.endsWith(".js")) {
     const p = path.join(__dirname, url);
     ctx.type = "text/javascript";
     ctx.body = fs.readFileSync(p, "utf-8");
